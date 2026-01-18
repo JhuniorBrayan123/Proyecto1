dc0dd2e004f5251bfc9139aa7cd8e5c4b9ae856e
Jhunior Brayan Gutierrez <gutierrezmamanib26@gmail.com>
2026-01-18 00:44:19 -0500

    Primera plantilla de angular con talwindcss

--- nuevo-dashboard/src/app/app.routes.ts
+++ nuevo-dashboard/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Services } from './shared/services/services';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'services', component: Services },
+  { path: '', redirectTo: 'services', pathMatch: 'full' },
+];
