1e67138521e26cb9e0dfe2eeb9e3315d69b85330
Jhunior Brayan Gutierrez <gutierrezmamanib26@gmail.com>
2026-01-25 01:49:43 -0500

    Ultimo fix antes de pasar al login

--- nuevo-dashboard/src/app/shared/components/layout/layout.ts
+++ nuevo-dashboard/src/app/shared/components/layout/layout.ts
@@ -4,10 +4,11 @@ import { Header } from '../header/header';
 import { Sidebar } from '../sidebar/sidebar';
 import { Footer } from '../footer/footer';
 
+
 @Component({
   selector: 'app-layout',
   imports: [Header, Sidebar, Footer, RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export default class Layout {}
+export  class Layout {}
